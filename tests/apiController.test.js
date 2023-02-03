6cafbf82f749be2eb3a55439dde2376ca2726910
Kushagraved <kushagraved@gmail.com>
2023-02-03 16:11:13 +0530

    fix: testing of controller

--- tests/apiController.test.js
+++ tests/apiController.test.js
@@ -1,28 +1,24 @@
 const apiServices = require('../src/services/apiServices');
-
-
+const {getCompanyById}=require('../src/controllers/getCSVController');
 describe('get company by id',() => {
   it('should return company details', async () => {
-    jest.spyOn(apiServices, 'getCompanyByIdService').mockResolvedValue([{
-    }]);
-    const mockReq = {};
+    jest.spyOn(apiServices, 'getCompanyByIdService').mockResolvedValue({
+      name:'Micrsoft',
+    });
+    const mockReq = {
+      params:{
+        id:2
+      }
+    };
     const mockRes = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
     }; 
-    await categoryController.getCategories(mockReq, mockRes);
+    await getCompanyById(mockReq, mockRes);
     expect(mockRes.status).toHaveBeenCalledWith(200);
-    expect(mockRes.json).toHaveBeenCalledWith([{
-      'id': 1,
-      'name': 'Electronics',
-      'description': 'Electronics',
-      'attributes': [
-        'color',
-        'size'
-      ],
-      'createdAt': '2021-02-01T07:19:12.424Z',
-      'updatedAt': '2021-02-01T07:19:12.424Z'
-    }]);
+    expect(mockRes.json).toHaveBeenCalledWith({
+      name:'Micrsoft',
+    });
   });
 });
 
